5faded4df3518a23466d3d0ba9b7b7d837e9ca30
venkatram64 <venkat.veerareddy@gmail.com>
2017-11-28 08:28:09 -0500

    added code

--- src/js/actions/tweetsActions.js
+++ src/js/actions/tweetsActions.js
@@ -3,13 +3,7 @@ import axios from "axios";
 export function fetchTweets() {
   return function(dispatch) {
     dispatch({type: "FETCH_TWEETS"});
-    
-    /* 
-      http://rest.learncode.academy is a public test server, so another user's experimentation can break your tests
-      If you get console errors due to bad data:
-      - change "reacttest" below to any other username
-      - post some tweets to http://rest.learncode.academy/api/yourusername/tweets
-    */
+   
     axios.get("http://rest.learncode.academy/api/reacttest/tweets")
       .then((response) => {
         dispatch({type: "FETCH_TWEETS_FULFILLED", payload: response.data})
--- src/js/store.js
+++ src/js/store.js
@@ -6,5 +6,6 @@ import promise from 'redux-promise-middleware';
 import reducer from './reducers';
 
 const middleware = applyMiddleware(promise(), thunk, logger());
-
+//thunk is for async calls to dipatch
+//promise is for to use promise to call rest calls
 export default createStore(reducer, middleware);
